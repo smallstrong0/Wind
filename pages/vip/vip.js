d1d632ea1eab2ec62bc53cd300c55320dc8a8dd7
smallstrong0 <393019766@qq.com>
2017-12-09 23:07:18 +0800

    link of wechat pay

--- pages/vip/vip.js
+++ pages/vip/vip.js
@@ -7,79 +7,67 @@ Page({
   data: {
     grids: [
       {
-        text: "1￥=10积分"
+        text: "1￥=10积分",
+        money:1,
       },
       {
-        text: "5￥=55积分"
+        text: "5￥=55积分",
+        money: 5,
       },
       {
-        text: "10￥=110积分"
+        text: "10￥=110积分",
+        money: 10,
       },
       {
-        text: "20￥=230积分"
+        text: "20￥=230积分",
+        money: 20,
       },
       {
-        text: "50￥=600积分"
-      },
-      {
-        text: "100￥=1300积分"
+        text: "50￥=600积分",
+        money: 50,
       },
     ]
+  }, 
+  vip: function (e) {
+    var money = e.currentTarget.dataset.money; //打印可以看到，此处已获取到了对应的id
+    console.log(money)
+    wx.getStorage({
+      key: 'user_data',
+      success: function (res) {
+        wx.request({
+          url: "https://smallstrong.site/api/add_coin",
+          method: 'POST',
+          data: {
+            'user_id': res.data.data.user_id,
+            'money': money,
+            'openid': res.data.data.openid,
+          },
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          success: function (res) {
+            console.log(res)
+            wx.requestPayment({
+              'timeStamp': res.data.data.timeStamp+'',
+              'nonceStr': res.data.data.nonceStr+'',
+              'package': res.data.data.package,
+              'signType': res.data.data.signType,
+              'paySign': res.data.data.paySign,
+              'success': function (res) {
+                console.log(res);
+              },
+              'fail': function (res) {
+                console.log('fail:' + JSON.stringify(res));
+              }
+            })
+          },
+          fail: function (err) {
+            console.log(err)
+          }
+        })
+      }
+    })
+    
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
