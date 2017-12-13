c1292948adf31454c0e5837d2f6643c14483014a
smallstrong0 <393019766@qq.com>
2017-12-13 09:05:53 +0800

    【API】api replace

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-//app.js
 var app = getApp();
-var baseURL = "https://smallstrong.site/api/";
+const api = require('./utils/api.js');
+
 App({
-  onLaunch: function (options) {
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: baseURL+'login?code='+res.code,
+    onLaunch: function (options) {
+        wx.login({
             success: function (res) {
-              console.log(res.data)
-              wx.setStorage({
-                key: "user_data",
-                data: res.data
-              })
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      },
-     
-    });
+                if (res.code) {
+                    api.login({
+                        data: {'code': res.code},
+                        success: (res) => {
+                            console.log(res.data)
+                            wx.setStorage({
+                                key: "user_data",
+                                data: res.data
+                            })
+                        }
+                    })
 
-    wx.getUserInfo({
-      success: function (res) {
-        wx.setStorage({
-          key: "user_info",
-          data: res.userInfo
+                } else {
+                    console.log('获取用户登录态失败！' + res.errMsg)
+                }
+            },
+
+        });
+
+        wx.getUserInfo({
+            success: function (res) {
+                wx.setStorage({
+                    key: "user_info",
+                    data: res.userInfo
+                })
+                var value = wx.getStorageSync('user_data')
+                if (value) {
+                    console.log(value)
+                    api.mod_user_info({
+                        data: {
+                            'user_id': value.data.user_id,
+                            'user_info': JSON.stringify(res.userInfo),
+                        },
+                        success: (res) => {
+                            console.log(res.data)
+                        }
+                    })
+                }
+
+            }
         })
-        var value = wx.getStorageSync('user_data')
-          if (value) {
-            console.log(value)
-          //发起网络请求
-          wx.request({
-            url: baseURL + 'mod_user_info?user_id=' + value.data.user_id + '&user_info=' + JSON.stringify(res.userInfo),
-            success: function (res) {
-              console.log(res.data)
-            }
-          })
-          }
-         
-      }
-    })
-  },
+    },
 
-  
-  
+
 })
