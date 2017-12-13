c8c038eb9383ad43925590ea192b4bc1bd93f969
smallstrong0 <393019766@qq.com>
2017-12-13 09:05:10 +0800

    【FUNC】core of api

---
+++ utils/api.js
@@ -0,0 +1,42 @@
+const apiURL = 'https://smallstrong.site/api';
+
+const wxRequest = (params, url) => {
+    wx.request({
+        url: url,
+        method: params.method || 'GET',
+        data: params.data || {},
+        header: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        success(res) {
+            if (params.success) {
+                params.success(res);
+            }
+        },
+        fail(res) {
+            if (params.fail) {
+                params.fail(res);
+            }
+        },
+        complete(res) {
+            if (params.complete) {
+                params.complete(res);
+            }
+        },
+    });
+};
+
+const login = (params) => {
+    wxRequest(params, `${apiURL}/login`);
+};
+
+const mod_user_info = (params) => {
+    wxRequest(params, `${apiURL}/mod_user_info`);
+};
+
+
+module.exports = {
+    login,
+    mod_user_info,
+};
